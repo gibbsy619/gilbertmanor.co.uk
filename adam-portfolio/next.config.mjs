46125e27c288ab3a30c7c2abe63309d067adf615
adamgibb <adamgilbert81537@gmail.com>
2025-03-10 00:43:48 +0000

    Major update to personal website

--- adam-portfolio/next.config.mjs
+++ adam-portfolio/next.config.mjs
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   basePath: isProd ? '/gilbertmanor.co.uk' : '',
   assetPrefix: isProd ? '/gilbertmanor.co.uk/' : '',
+  output: 'export',
 };
 
 export default nextConfig;
